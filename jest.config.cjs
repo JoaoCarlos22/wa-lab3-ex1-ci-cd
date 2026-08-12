5f7e8f0de9e7fc1b084cb705dfb517c8cd11d5c9
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2026-08-12 14:30:19 -0400

    test: teste unitario add

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: false,
+      tsconfig: {
+        module: 'commonjs',
+      },
+    }],
+  },
+};
